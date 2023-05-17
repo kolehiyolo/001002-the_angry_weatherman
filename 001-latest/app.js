214ee3f505ddc1e72737bcd22818f18a10bba57e
Tristan Sean Paul Perfecto Cinco <71287768+kolehiyolo@users.noreply.github.com>
2023-05-18 03:10:18 +0800

    Cloud Generator Working

--- 001-latest/app.js
+++ 001-latest/app.js
@@ -1,11 +1,14 @@
 //jshint esversion:6
 
+// * MERN Essentials
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const https = require("https");
-const url = require("url");
+
+
+// const https = require("https");
+// const url = require("url");
 
 const app = express();
 
@@ -15,80 +18,54 @@ app.use(bodyParser.urlencoded({
     extended: true
 }))
 
-mongoose.connect("mongodb://127.0.0.1:27017/theAngryWeathermanDB", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
+// mongoose.connect("mongodb://127.0.0.1:27017/theAngryWeathermanDB", {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// });
 
-const weatherHTTPSPrefix = `https://api.openweathermap.org/data/3.0/onecall?`;
-const openweathermapKey = `092e0f7f9c74b92b1d8ba0cd87abf2b7`;
-let weatherResults;
-
-// const userSchema = {
-//     email: String,
-//     password: String
-// };
-
-// const User = new mongoose.model("User", userSchema);
+// const weatherHTTPSPrefix = `https://api.openweathermap.org/data/3.0/onecall?`;
+// const openweathermapKey = `092e0f7f9c74b92b1d8ba0cd87abf2b7`;
+// let weatherResults;
 
 app.get("/", function (req, res) {
     console.log(`GET /`);
 
-    res.render("pages/home", {
-        page: "home",
-        showingResults: false,
-    });
+    res.render("pages/home");
 });
 
 app.get("/weather", function (req, res) {
-    res.render("pages/weather", {
-        page: "weather",
-        showingResults: false,
-        weatherResults: weatherResults
-    });
+    res.render("pages/weather");
 });
 
-app.get("/weather/:city", function (req, res) {
-    console.log(`GET /weather/${req.params.city}`);
+// app.get("/weather/:city", function (req, res) {
+//     console.log(`GET /weather/${req.params.city}`);
 
-    console.log(req.params.city);
+//     console.log(req.params.city);
 
-    const city = req.params.city;
-    const weatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${openweathermapKey}`;
+//     const city = req.params.city;
+//     const weatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${openweathermapKey}`;
 
-    https.get(weatherURL, function (response) {
-        console.log(response.statusCode);
+//     https.get(weatherURL, function (response) {
+//         console.log(response.statusCode);
 
-        response.on("data", function (data) {
-            res.send(JSON.parse(data));
-            // res.render("pages/weather", {
-            //     showingResults: true,
-            //     weatherResults: JSON.parse(data)
-            // });
-        });
-    });
+//         response.on("data", function (data) {
+//             res.send(JSON.parse(data));
+//             // res.render("pages/weather", {
+//             //     showingResults: true,
+//             //     weatherResults: JSON.parse(data)
+//             // });
+//         });
+//     });
 
-});
+// });
 
-app.post("/weather/", function (req, res) {
-    console.log(`POST /weather`);
+// app.post("/weather/", function (req, res) {
+//     console.log(`POST /weather`);
 
-    console.log(req.body.city);
+//     console.log(req.body.city);
 
-    res.redirect(`/weather/${req.body.city}`);
-});
-
-app.get("/holidays", function (req, res) {
-    console.log(`GET /holidays`);
-
-    res.render("pages/holidays");
-});
-
-app.get("/animals", function (req, res) {
-    console.log(`GET /animals`);
-
-    res.render("pages/animals");
-});
+//     res.redirect(`/weather/${req.body.city}`);
+// });
 
 app.listen(3000, function () {
     console.log(`Server started on port 3000`);
---
+++ 001-latest/public/javascript/generate-clouds.js
@@ -0,0 +1,91 @@
+console.log(`READ generate-clouds.js`);
+
+function generateClouds(count) {
+    console.log(`RUN generateClouds()`);
+
+    const cloudProps = decideProperties();
+    const inlineStyle = `
+        width: ${cloudProps.size}px;
+        height: ${cloudProps.size}px;
+        opacity: ${cloudProps.opacity};
+        transform: rotate(${cloudProps.rotation}deg);
+        animation: cloudDrift-${count} ${cloudProps.speed}s linear 0s infinite alternate;
+    `;
+
+    const cloud = `<div class="cloud" style="${inlineStyle}"></div>`;
+
+    var createdStyleTag = document.createElement("style");
+    createdStyleTag.textContent = 
+    `@keyframes cloudDrift-${count}{
+        from {
+            left: ${cloudProps.origin.positionX}px;
+            top: ${cloudProps.origin.positionY}px;
+        }
+        to {
+            left: ${cloudProps.destination.positionX}px;
+            top: ${cloudProps.destination.positionY}px;
+        }
+    };`;
+
+    document.body.appendChild(createdStyleTag);
+
+    $(`.decoratives`).append(cloud);
+}
+
+function decideProperties() {
+    const size = {
+        min: 50,
+        max: 750,
+    }
+
+    function findPosition(axis) {
+        // * First, we find the current screen width
+        // * Next, we find a value to add
+        // * Then, we decide whether to generate from the left or right side of the screen
+        // * Then, we calculate based on that decision
+        let position;
+        let screenValue = (axis === `x`) ? window.screen.width : window.screen.height;
+        // let addedValue = Math.floor(Math.random() * screenValue);
+        let addedValue = Math.floor(Math.random() * 1000);
+        let originPositive = (Math.random() * 1 < 0.5) ? false : true;
+
+        position = (originPositive) ? screenValue + addedValue : 0 - addedValue;
+
+        // position = (originPositive) ? screenValue - addedValue : 0 + addedValue;
+
+        return position;
+
+        // * Compressed
+        // return (((Math.random() * 1 < 0.5) ? `left` : `right`) === `left`) ? 0 - Math.floor(Math.random() * 1000) : screen.width + Math.floor(Math.random() * 1000);
+    }
+
+    let cloudProps = {
+        origin: {
+            positionX: findPosition('x'),
+            positionY: findPosition('y'),
+        },
+        destination: {
+            positionX: findPosition('x'),
+            positionY: findPosition('y'),
+        },
+        size: size.min + Math.floor(Math.random() * (size.max - size.min)),
+        rotation: Math.floor(Math.random() * 360),
+        // direction: Math.floor(Math.random() * 360),
+        speed: 50 + Math.random() * 50,
+        opacity: Math.random() * 0.7,
+    }
+
+    return cloudProps;
+}
+
+generateClouds();
+
+let cloudLimit = 50;
+let cloudCount = 0;
+
+setInterval(() => {
+    if (cloudCount < cloudLimit) {
+        generateClouds(cloudCount);
+        cloudCount++;
+    }
+}, 0);
